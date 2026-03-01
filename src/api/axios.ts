51d418d0bed0474d3c50a8480f016ef071958f4c
Yuvraj-Prasad <lalouchn64@gmail.com>
2026-03-01 17:54:28 +0530

    Added deployed backend (Render)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "https://my-taskmanager-backend.onrender.com"
+    baseURL: "https://smart-taskmanager-backend-1.onrender.com"
 });
 
 export default API;
